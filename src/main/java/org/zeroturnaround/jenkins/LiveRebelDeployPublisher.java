c0a3be5f115503a52ef6485215363574733c265d
Juri Timošin <juri.timoshin@zeroturnaround.com>
2011-06-14 15:13:56 +0300

    uploading 1 artifact after the build, using lr-api works.

--- src/main/java/org/zeroturnaround/jenkins/LiveRebelDeployPublisher.java
+++ src/main/java/org/zeroturnaround/jenkins/LiveRebelDeployPublisher.java
@@ -1,11 +1,16 @@
 package org.zeroturnaround.jenkins;
 
+import com.zeroturnaround.liverebel.api.*;
+import com.zeroturnaround.liverebel.api.Error;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
-import hudson.tasks.*;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Notifier;
+import hudson.tasks.Publisher;
 import hudson.util.FormValidation;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -13,21 +18,18 @@ import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 import javax.servlet.ServletException;
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Serializable;
 
 public class LiveRebelDeployPublisher extends Notifier implements Serializable {
 
 	private final String artifact;
-	private final String appName;
 
 	// Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
 	@DataBoundConstructor
-	public LiveRebelDeployPublisher(String artifact, String appName) {
+	public LiveRebelDeployPublisher(String artifact) {
 		this.artifact = artifact;
-		this.appName = appName;
 	}
 
 	/**
@@ -36,29 +38,56 @@ public class LiveRebelDeployPublisher extends Notifier implements Serializable {
 	public String getArtifact() {
 		return artifact;
 	}
-	public String getAppName() {
-		return appName;
-	}
 
 	@Override
 	public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
 		try {
-			
-
-
-			Process proc=Runtime.getRuntime().exec("/home/jt/zt/Test/jenkins-plugin/liverebel-plugin/upload-activate-version.sh /home/jt/zt/Test/jenkins-plugin/liverebel-deploy-plugin/work/jobs/lr-demo-app/workspace/"+artifact+" "+ appName);
-			BufferedReader read=new BufferedReader(new InputStreamReader(proc.getInputStream()));
-			proc.waitFor();
-			while(read.ready()) {
-				listener.getLogger().println(read.readLine());
-			}
+			CommandCenter commandCenter = new CommandCenterFactory().
+				setUrl(getDescriptor().getLrUrl()).
+				setVerbose(true).
+				authenticate(getDescriptor().getAuthToken()).
+				newCommandCenter();
+				UploadInfo upload = commandCenter.upload(new File(build.getWorkspace().child(artifact).toURI()));
+				listener.getLogger().println("SUCCESS: " + upload.getApplicationId() + " " + upload.getVersionId() + " was uploaded.");
 		}
-		catch (IOException e){
-			listener.getLogger().println(e.getStackTrace().toString());
+		catch (IllegalArgumentException e) {
+			listener.getLogger().println("ERROR! " + e.getMessage());
 			return false;
 		}
-		catch (InterruptedException e) {
-			listener.getLogger().println(e.getStackTrace().toString());
+		catch (Conflict e) {
+			listener.getLogger().println("ERROR! " + e.getMessage());
+			return false;
+		}
+		catch (Forbidden e) {
+			listener.getLogger().println("ERROR! Access denied, invalid username or password.");
+			return false;
+		}
+		catch (Error e) {
+			listener.getLogger().println("ERROR! Unexpected error received from server.");
+			listener.getLogger().println();
+			listener.getLogger().println("URL: " + e.getURL());
+			listener.getLogger().println("Status code: " + e.getStatus());
+			listener.getLogger().println("Nessage: " + e.getMessage());
+			return false;
+		}
+		catch (ParseException e) {
+			listener.getLogger().println("ERROR! Unable to read server response.");
+			listener.getLogger().println();
+			listener.getLogger().println("Response: " + e.getResponse());
+			listener.getLogger().println("Reason: " + e.getMessage());
+			return false;
+		}
+		catch (ConnectException e) {
+			listener.getLogger().println("ERROR! Unable to connect to server.");
+			listener.getLogger().println();
+			listener.getLogger().println("URL: " + e.getURL());
+			listener.getLogger().println("Reason: " + e.getMessage());
+			return false;
+		}
+		catch (Throwable t) {
+			listener.getLogger().println("ERROR! Unexpected error occured:");
+			listener.getLogger().println();
+			t.printStackTrace(listener.getLogger());
 			return false;
 		}
 		return true;
@@ -77,18 +106,12 @@ public class LiveRebelDeployPublisher extends Notifier implements Serializable {
 	@Extension // This indicates to Jenkins that this is an implementation of an extension point.
 	public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
-		private String authToken;
-		private String lrRunningAddress;
-
-		public FormValidation doCheckAppName(@QueryParameter String value) throws IOException, ServletException {
-			if (value.length() == 0)
-				return FormValidation.error("Please set application name");
-			return FormValidation.ok();
-		}
+		private String authToken = "9248c21e-27fc-4d9b-9df3-d7b08c51aee2";
+		private String lrUrl = "https://localhost:9001";
 
 		public FormValidation doCheckArtifact(@QueryParameter String value) throws IOException, ServletException {
 			if (value.length() == 0)
-				return FormValidation.error("Please set artifact");
+				return FormValidation.error("Please, set artifact");
 			return FormValidation.ok();
 		}
 
@@ -109,7 +132,7 @@ public class LiveRebelDeployPublisher extends Notifier implements Serializable {
 			// To persist global configuration information,
 			// set that to properties and call save().
 			authToken = formData.getString("authToken");
-			lrRunningAddress = formData.getString("lrRunningAddress");
+			lrUrl = "https://" + formData.getString("lrUrl").replaceFirst("http://", "").replaceFirst("https://", "");
 			save();
 			return super.configure(req,formData);
 		}
@@ -117,8 +140,8 @@ public class LiveRebelDeployPublisher extends Notifier implements Serializable {
 		public String getAuthToken() {
 			return authToken;
 		}
-		public String getLRRunningAddress(){
-			return lrRunningAddress;
+		public String getLrUrl(){
+			return lrUrl;
 		}
 	}
 }
